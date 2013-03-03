7b2bc8e6977c21b9660e2041603c4e48f97c5795
Albert <albert@albert-Satellite-A300.(none)>
2013-03-03 23:03:49 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CforWeb::Application.config.secret_token = '1b0a196b449450672a0a57cbf357bcfccee2743f5ba317232542250c20562e08ad9e4eb2f43d2ec0ff55f4a00974c0f9759467434d9f22ae94d02caa7ec01163'
